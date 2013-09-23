ea01a96037c4aa460d44580216d22fd0bbae750a
jq1992411 <991540504@qq.com>
2013-09-23 15:20:56 +0800

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * 单入口
+     * @weibo   weibo.com/cameljq
+     * @website charlesjiang.com
+     */
+    define('PHP4Y','./PHP4Y/'); //框架目录
+    define('APP_NAME','home');  //当前应用名称
+    require'./PHP4Y/PHP4Y.php'; //导入框架文件
+
